830d3b83a685ea3aeca1ea46816ac2511b77220c
Santona Subedi <santona.subedi@fastsw.com>
2024-01-17 10:31:11 -0600

    latest notes

--- School.cs
+++ School.cs
@@ -1,4 +1,4 @@
-class School{
+ class School{
     int schoolID;
     int foundDate;
 
@@ -7,4 +7,15 @@ class School{
         foundDate = _founfDate
 
     }
+
+    //static - meaning it does not belong to the object, no reference (this) to static method
+    public static void Main(){
+        School sc1 = new School(1, 1998);
+        /* although this statement is syntatically correct, C# compiler acts as there is not such field schoolID in the object in the first place.
+          - that field and other field are inside School and declared as private, not visible from inside when there is no access modifier 
+          - "public" in the School constructor
+        */
+        sc1.schoolID = 2; 
+    }
+
 }
